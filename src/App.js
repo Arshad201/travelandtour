95004bbbbe91ca647700a785d52186408602e8a5
Mushafiya <kmushafiya003@gmail.com>
2023-06-06 17:06:54 +0000

    Add video on Home Page

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './Component/Navbar/Navbar.js';
 import Footer from './Component/Footer/Footer.js';
+import Home from './Component/Home/Home'
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-      <Routes>
-        <Route exact path='/' element={<h1>Hello Mushafiya, How are you?</h1>}/>
-      </Routes>
-    <Footer/>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Home></Home>} />
+        </Routes>
+      </div>
+      <Footer />
     </>
   )
 }
