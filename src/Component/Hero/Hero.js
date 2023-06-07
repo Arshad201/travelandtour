e32c3fc398e2514e3d833d8346ae207e919aee33
Mushafiya <kmushafiya003@gmail.com>
2023-06-07 22:20:55 +0000

    Completed Hero section with Responsiveness

--- src/Component/Hero/Hero.js
+++ src/Component/Hero/Hero.js
@@ -2,32 +2,38 @@ import React from 'react'
 import video from '../../assets/ocean.mp4'
 import './Hero.css'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import { GrLocation } from 'react-icons/gr'
+import {FaFilter} from 'react-icons/fa'
+import {FiFacebook} from 'react-icons/fi'
+import {FiInstagram} from 'react-icons/fi'
+import {FiTwitter} from 'react-icons/fi'
 
 const Hero = () => {
 
-    const [formdata,setFormdata]=useState({
-        destination : " ",
-        date : "",
-        price:""
+    const [formdata, setFormdata] = useState({
+        destination: " ",
+        date: "",
+        price:1000
 
     })
 
-    function changeHandler(events){
+    function changeHandler(events) {
 
-        setFormdata(prevData=>{
-            return({
+        setFormdata(prevData => {
+            return ({
                 ...prevData,
-                [events.target.name] : events.target.value
+                [events.target.name]: events.target.value
             })
         })
 
     }
-    function submitHandler(events){
+    function submitHandler(events) {
         events.preventDefault();
         console.log(formdata);
     }
 
-    
+
     return (
         <div className='hero-container'>
             <div className='video-container'>
@@ -37,41 +43,65 @@ const Hero = () => {
             <div className='hero-content'>
                 <h2 className='hero-heading1'>Our Packages</h2>
                 <h1 className='hero-heading2'>Search your Holiday</h1>
-                <form  className='form-container' onSubmit={submitHandler}>
+                <form className='form-container' onSubmit={submitHandler}>
                     <label  >
                         <p >Search Your Destination</p>
-                        <input type="text"
-                            placeholder='Enter name here...'
-                            name='destination'
-                            value={formdata.destination}
-                            onChange={changeHandler}
-                             />
+                        <div className='destination-container'>
+                            <input type="text"
+                                className='destination'
+                                placeholder='Enter name here...'
+                                name='destination'
+                                value={formdata.destination}
+                                onChange={changeHandler}
+
+                            />
+                            <span className='location-container'>
+                                <GrLocation className='location-icon'></GrLocation>
+                            </span>
+                        </div>
                     </label>
                     <label>
                         <p>Search your date</p>
                         <input type="date"
-                         name='date'
-                         value={formdata.date}
-                         onChange={changeHandler}
-                         />
+                            name='date'
+                            value={formdata.date}
+                            onChange={changeHandler}
+                        />
                     </label>
                     <label >
                         <div className='price-container'>
                             <p>Max price</p>
-                            <span className='max-price'>$5000</span>
+                            <span className='max-price'>${formdata.price}</span>
                         </div>
                         <input type="range"
-                         min="1000" 
-                         max="5000" 
-                         name='price'
-                         value={formdata.price}
-                         onChange={changeHandler}
-                         />
-                    </label>
-                    name='destination'
-                            value={formdata. destination}
+                            min="1000"
+                            max="5000"
+                            name='price'
+                            value={formdata.price}
                             onChange={changeHandler}
+                        />
+                    </label>
+
+                    <div className='filter'>
+                         <FaFilter className='filter-icon'></FaFilter>
+                         <p className='filter-text'>More Filters</p>
+                    </div>
+                  
                 </form>
+                <div className='icon-group'>
+                    <Link to="https://www.facebook.com">
+                    <FiFacebook className='facebook icon'></FiFacebook>
+                    </Link>
+
+                    <Link to="https://www.instagram.com">
+                    <FiInstagram className='instagram icon'></FiInstagram>
+                    </Link>
+
+                    <Link to="https://www.twitter.com">
+                        <FiTwitter className='twitter icon'></FiTwitter>
+                    </Link>
+
+                </div>
             </div>
         </div>
     )
